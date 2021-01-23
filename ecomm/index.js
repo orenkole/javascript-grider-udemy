548b894d59044c983364c9deaea59703107a4078
Oleh <orenkole@gmail.com>
2021-01-23 20:19:30 +0200

    356. Understanding Form Submissions

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,7 +3,22 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi there!")
+  res.send(
+    `
+      <div class="">
+      <form method="POST">
+        <input type="email" placeholder="email" name="email">
+        <input type="password" placeholder="password" name="password">
+        <input type="password" placeholder="password confirmataion" name="password confirmation">
+        <button>Sign Up</button>
+      </form>
+    </div>
+    `
+  )
+})
+
+app.post("/", (req, res) => {
+  res.send("account created")
 })
 
 app.listen(3000, () => {
