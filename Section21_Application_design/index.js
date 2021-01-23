723e107ee2455b0596a8bd371c9052e2d1cf3e74
Oleh <orenkole@gmail.com>
2021-01-23 15:54:23 +0200

    228. Delaying Search Input

--- Section21_Application_design/index.js
+++ Section21_Application_design/index.js
@@ -1,13 +1,25 @@
 // 6146d12e
-
-const fetchData = async () => {
+console.log("hello time");
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "6146d12e",
-      s: "avengers"
+      s: searchTerm
     }
   })
   console.log(response.data);
 }
 
-fetchData();
+let timeoutId;
+onInput = (event) => {
+  if(timeoutId) {
+    console.log(timeoutId);
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value)
+  }, 2000);
+}
+
+const input = document.querySelector("input");
+input.addEventListener("input", onInput)
