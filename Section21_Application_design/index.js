7eea399409d8bc244bc2741e7df5ba172ea6cd00
Oleh <orenkole@gmail.com>
2021-03-06 13:08:00 +0200

    230. Implementing a Reusable Debounce

--- Section21_Application_design/index.js
+++ Section21_Application_design/index.js
@@ -1,18 +1,6 @@
 // 6146d12e
 const input = document.querySelector("input");
 
-const debounce = (func, delay = 500) => {
-  let timeoutId;
-  return (...args) => {
-    if(timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  }
-}
-
 const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
