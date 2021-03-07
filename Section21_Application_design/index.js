6fe9fafb57062a5ac8c7b1a6ee6f87b4aa4b5c84
Oleh <orenkole@gmail.com>
2021-03-07 17:58:20 +0200

    237. Moving HTML Generation

--- Section21_Application_design/index.js
+++ Section21_Application_design/index.js
@@ -1,5 +1,5 @@
 // 6146d12e
-const input = document.querySelector("input");
+
 
 const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
@@ -14,15 +14,31 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 }
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label for=""><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
 onInput = async (event) => {
   const movies = await fetchData(event.target.value);
+  dropdown.classList.add("is-active");
   for(let movie of movies) {
-    const div = document.createElement("div")
-    div.innerHTML = `
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+      ${movie.Title}
     `;
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.append(option);
   }
 }
 
