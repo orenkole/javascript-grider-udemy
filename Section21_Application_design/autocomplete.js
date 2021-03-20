d1a970fecba89a2b9610eb693b0f381fb6f78cb8
Oleh <orenkole@gmail.com>
2021-03-20 16:31:16 +0200

    250. Extracting Rendering Logic. From config object we get a function that returns HTML that has to be inside automplete item in autocomplete.js

--- Section21_Application_design/autocomplete.js
+++ Section21_Application_design/autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({root}) => {
+const createAutoComplete = ({root, renderOption}) => {
   // const root = document.querySelector(".autocomplete");
   root.innerHTML = `
     <label for=""><b>Search For a Movie</b></label>
@@ -23,12 +23,8 @@ const createAutoComplete = ({root}) => {
     dropdown.classList.add("is-active");
     for(let movie of movies) {
       const option = document.createElement("a");
-      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster
       option.classList.add("dropdown-item");
-      option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-      `;
+      option.innerHTML = renderOption(movie);
       option.addEventListener("click", () => {
         // update input and close dropdown
         dropdown.classList.remove("is-active");
--- Section21_Application_design/index.js
+++ Section21_Application_design/index.js
@@ -15,13 +15,14 @@ const fetchData = async (searchTerm) => {
 }
 
 createAutoComplete({
-  root: document.querySelector("autocomplete-1")
-})
-createAutoComplete({
-  root: document.querySelector("autocomplete-2")
-})
-createAutoComplete({
-  root: document.querySelector("autocomplete-3")
+  root: document.querySelector(".autocomplete"),
+  renderOption: (movie) => {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster
+    return `
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
+  `;
+  }
 })
 
 const onMovieSelect = async (movie) => {
