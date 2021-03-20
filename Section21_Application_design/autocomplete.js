f643e1e702c2b6a276719f894478751f9df78855
Oleh <orenkole@gmail.com>
2021-03-20 16:11:26 +0200

    249. Displaying Multiple Autocompletes

---
+++ Section21_Application_design/autocomplete.js
@@ -0,0 +1,49 @@
+const createAutoComplete = ({root}) => {
+  // const root = document.querySelector(".autocomplete");
+  root.innerHTML = `
+    <label for=""><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
+  const input = root.querySelector("input");
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
+
+  onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+    if(!movies.length) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+    resultsWrapper.innerHTML = "";
+    dropdown.classList.add("is-active");
+    for(let movie of movies) {
+      const option = document.createElement("a");
+      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster
+      option.classList.add("dropdown-item");
+      option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+      `;
+      option.addEventListener("click", () => {
+        // update input and close dropdown
+        dropdown.classList.remove("is-active");
+        input.value = movie.Title;
+        onMovieSelect(movie);
+      })
+      resultsWrapper.append(option);
+    }
+  }
+
+  input.addEventListener("input", debounce(onInput, 1000))
+
+  document.addEventListener("click", (e) => {
+    if(!root.contains(e.target)) {
+      dropdown.classList.remove("is-active");
+    }
+  })
+}
--- Section21_Application_design/index.js
+++ Section21_Application_design/index.js
@@ -14,62 +14,24 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 }
 
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-  <label for=""><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-  if(!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-  resultsWrapper.innerHTML = "";
-  dropdown.classList.add("is-active");
-  for(let movie of movies) {
-    const option = document.createElement("a");
-    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-      <img src="${imgSrc}" />
-      ${movie.Title}
-    `;
-    option.addEventListener("click", () => {
-      // update input and close dropdown
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    })
-    resultsWrapper.append(option);
-  }
-}
-
-input.addEventListener("input", debounce(onInput, 1000))
-
-document.addEventListener("click", (e) => {
-  if(!root.contains(e.target)) {
-    dropdown.classList.remove("is-active");
-  }
+createAutoComplete({
+  root: document.querySelector("autocomplete-1")
+})
+createAutoComplete({
+  root: document.querySelector("autocomplete-2")
+})
+createAutoComplete({
+  root: document.querySelector("autocomplete-3")
 })
 
 const onMovieSelect = async (movie) => {
-  const reponse = await axios.get(`http://www.omdbapi.com/`, {
+  const response = await axios.get(`http://www.omdbapi.com/`, {
     params: {
       apikey: "6146d12e",
       i: movie.imdbID,
     }
   })
-  document.querySelector("#summary").innerHTML = movieTemplate(reponse.data)
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data)
 }
 
 const movieTemplate = (movieDetail) => {
